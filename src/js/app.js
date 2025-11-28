d1843fffc1054aa717957715443d2eec3191845f
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-11-28 18:45:47 +0000

    nombre

--- src/js/app.js
+++ src/js/app.js
@@ -33,8 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
+          <h1>${variables.name ? variables.name : "Name"} ${
+    variables.lastName ? variables.lastName : "Last Name"
+  }</h1>
+          <h2>${
+            variables.socialMediaPosition
+              ? variables.socialMediaPosition
+              : "Social Media Pos"
+          }</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
